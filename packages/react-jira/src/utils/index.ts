4c1ca4342ef59c32f46b0a11709bf3597bded162
lijunfeng <junfeng.li@hand-china.com>
2021-11-21 22:30:29 +0800

    jira

---
+++ packages/react-jira/src/utils/index.ts
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
+
+// 在一个函数里，改变传入的对象本身是不好的
+export const cleanObject = (object: object) => {
+  // Object.assign({}, object)
+  const result = { ...object };
+  Object.keys(result).forEach((key) => {
+    // @ts-ignore
+    const value = result[key];
+    if (isFalsy(value)) {
+      // @ts-ignore
+      delete result[key];
+    }
+  });
+  return result;
+};
+
+export const useMount = (callback: () => void) => {
+  useEffect(() => {
+    callback();
+    return () => {
+
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+};
+
+// 后面用泛型来规范类型
+export const useDebounce = <V>(value: V, delay?: number) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    // 每次在value变化以后，设置一个定时器
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    // 每次在上一个useEffect处理完以后再运行
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+
+  return debouncedValue;
+};
