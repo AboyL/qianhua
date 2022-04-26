ff1eecaedda1fed19cb4d4976bad594725dae47a
lijunfeng <junfeng.li@hand-china.com>
2022-04-26 10:14:48 +0800

    feat: 注册子应用

--- packages/micro-main/src/main.js
+++ packages/micro-main/src/main.js
@@ -2,5 +2,21 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './index.css'
+import { registerMicroApps } from '@lixingzhi/trunk'
+
+registerMicroApps([
+  {
+    name: 'react16',// 唯一
+    entry: '//localhost:9002/',
+    container: '#micro-container',
+    activeRule: '/react16',
+  },
+  {
+    name: 'vue3',
+    entry: '//localhost:9003/',
+    container: '#micro-container',
+    activeRule: '/vue3',
+  }
+])
 
 createApp(App).use(router()).mount('#micro_web_main_app')
---
+++ packages/trunk/src/const/subApps.js
@@ -0,0 +1,7 @@
+let list = []
+
+export const getList = () => list
+
+window.__MICRO_GETLIST = getList
+
+export const setList = appList => list = appList
